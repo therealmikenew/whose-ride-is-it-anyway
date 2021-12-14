2ea6ddc2120eff5daedf222d51a3fe2539699634
Michael Newman <therealmichaelnewman@gmail.com>
2021-12-13 19:20:07 -0500

    updates to ParksList.jsx

--- whose-ride-is-it-anyway/src/components/ParksList.jsx
+++ whose-ride-is-it-anyway/src/components/ParksList.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function ParksList() {
+
+    const [clicked, setClicked] = useState("♡")
+    const [count, setCount] = useState({park.likes}])
+
+    const handleLikes = () => {
+
+        if (clicked === "♡") {
+            setClicked("❤") 
+            setCount({park.likes}++)
+        } else {
+            setClicked("♡")
+            setCount({park.likes}++)
+
+        }
+        
+    }
+
+
     return (
          <div>(
         <h6>List of Parks</h6>
-
-
 
        {props.parksArr.map((park) => (
         <div className="park-card" onClick={() => showPark(park)}>
           <img style={{ display: "block" }} src={park.img} alt={park.name} />
           <h3>{park.name}</h3>
-          <p>Likes: {park.likes}</p>
-          <button>LIKE ICON</button>
+          <p>Likes: {count}</p>
+          <button onClick={handleLikes}>{clicked}</button>
           <p>Deaths: {park.deaths}</p>
         </div>
       ))}

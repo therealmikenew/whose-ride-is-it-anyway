e49da24a17ce0d417a24dd3ce4f68ea87413adc9
Joseph Venable <JJVenable@gmail.com>
2021-12-14 15:49:06 -0500

    redid photos, added in backend server pointing

--- parent/Hack_a_Thon/db/index.js
+++ parent/Hack_a_Thon/db/index.js
@@ -1,13 +1,26 @@
-const mongoose = require("mongoose");
-/// what is the /plantsDatabase connecting to? WE didn't pull the data, we have it in park/rides
-let MONGODB_URI = "mongodb://127.0.0.1:27017/parksDatabase";
+const mongoose = require('mongoose')
+require('dotenv').config() // Add this line
+
+// let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/todo_tracker'
+// //here?----------------
+export const BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? `${window.location.origin}/api`
+    : 'http://localhost:3001/api'
+// //----------------------
 mongoose
-  .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .connect(dbUrl, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: true
+  })
   .then(() => {
-    console.log("Successfully connected to MongoDB.");
+    console.log('Successfully connected to MongoDB.')
   })
   .catch((e) => {
-    console.error("Connection error", e.message);
-  });
-const db = mongoose.connection;
-module.exports = db;
+    console.error('Connection error', e.message)
+  })
+mongoose.set('debug', true)
+const db = mongoose.connection
+
+module.exports = db

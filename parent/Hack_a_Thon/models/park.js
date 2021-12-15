2e1720fc63cb39b6e8b117adc1f8c48391a1cf42
Michael Newman <therealmichaelnewman@gmail.com>
2021-12-14 19:20:16 -0500

    synergy working

--- parent/Hack_a_Thon/models/park.js
+++ parent/Hack_a_Thon/models/park.js
@@ -4,13 +4,13 @@ const Schema = mongoose.Schema
 const Park = new Schema(
     {
         name: { type: String, required: true },
-        hours: { type: String, required: true},
+        hours: { type: String, required: false},
         location: { type: String, required: true},
-        attractionsNumber: { type: String, required: true},
-        water: { type: String, required: true},
+        attractionsNumber: { type: String, required: false},
+        water: { type: String, required: false},
         image: { type: String, required: true },
-        deaths: { type: Number, required: true },
-        likes: { type: Number, required: true },
+        deaths: { type: Number, required: false },
+        likes: { type: Number, required: false },
         id: { type: Number, required: false}
     },
     { timestamps: true },

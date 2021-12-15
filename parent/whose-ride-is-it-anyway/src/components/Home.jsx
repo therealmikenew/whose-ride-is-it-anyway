4ef0f38682cd9384a974fb6416b756d173b4213e
CaseyStewart1 <caseystewartdesigns@mail.com>
2021-12-14 22:12:43 -0500

    Read Me filled in

--- parent/whose-ride-is-it-anyway/src/components/Home.jsx
+++ parent/whose-ride-is-it-anyway/src/components/Home.jsx
@@ -6,12 +6,12 @@ function Home(props) {
     <div className='homePage'>
       <div>
         <img 
-        style={{ display: 'center', maxWidth: '50%' }}
+        style={{  display: 'center', maxWidth: '50%',  border:'solid 5px rgb(98, 195, 164)'}}
         src="https://usercontent.one/wp/www.chernobyl.one/wp-content/uploads/2020/05/Swimming-City-Park-08.jpg"
         alt="park"
         />
       </div>
-      <h2 style={{ color: 'rgb(193, 29, 37)' }}> Your one stop shop for Amusement Parks and knowing how many people died in them! </h2>
+      <h2 style={{ color: 'rgb(193, 29, 37)' }}> Your one stop shop for Amusement Parks and their body counts! </h2>
     </div>
   )
 }
